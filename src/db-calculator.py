18b479eead0d6a846652488f8885ea9641210ae6
sambbaer <git@samuelbaer.ch>
2025-02-04 21:46:13 +0100

    Update db-calculator.py

--- src/db-calculator.py
+++ src/db-calculator.py
@@ -1,9 +1,18 @@
 from datenloader import daten
 from config import RbBreite, RbHohe, REfBreite, REfHohe
 
+AnzNutzen1 = 0
+AnzNutzen2 = 0
+optiNzRb1 = 0
+optiNzRb2 = 0
+optimalBreite = 0
+optimalHohe = 0
+
+
 def dbcalculation(maschinenname):
     """Berechnet, ob der gegebene Rohbogen in die Maschine passt oder eine Optimierung nötig ist."""
-    
+    global AnzNutzen1, AnzNutzen2, optimalBreite, optimalHohe
+
     maschine = daten.get(maschinenname, None)
     if maschine is None:
         raise ValueError(f"Maschine '{maschinenname}' nicht gefunden.")
@@ -35,3 +44,18 @@ def dbcalculation(maschinenname):
         optimalHohe = (max_hohe // REfBreite) * REfBreite + randHohe
 
     print(f"⚠️ Rohbogen passt nicht optimal. Bestmögliche nutzbare Fläche: {optimalBreite} x {optimalHohe} mm")
+
+def optimalformat():
+    """Berechnet, wie viele optimale Druckbogen aus dem Rohbogen geschnitten werden können."""
+    global optiNzRb1, optiNzRb2, optimalBreite, optimalHohe
+
+    optiNzRb1 = (RbBreite // optimalBreite) * (RbHohe // optimalHohe)
+    optiNzRb1 = (RbBreite // optimalHohe) * (RbHohe // optimalBreite)
+
+    # Sind mehr als 1 optimal Druckbogen möglich aus einem Rohbogen?
+    if optiNzRb1 > 1
+        print(f"ℹ️ Es können {optiNzRb1} Druckbogen im Format {optimalBreite} x {optimalHohe} mm aus dem Rohbogen geschnitten werden.")
+    else if optiNzRb2 > 1
+        print(f"ℹ️ Es können {optiNzRb2} Druckbogen im Format {optimalBreite} x {optimalHohe} mm aus dem Rohbogen geschnitten werden.")
+    else
+        print(f"ℹ️ Das Druckformat {optimalBreite} x {optimalHohe} mm wird nicht empfohlen, da nur ein Druckbogen aus einem Rohbogen geschnitten werden kann.")
